961f5195e73bce0446ddd673d128456d91217f1c
Maimoona Khilji <64362437+MaimoonaKhilji@users.noreply.github.com>
2022-10-27 11:09:27 -0700

    Add files via upload

---
+++ arithmetic.sh
@@ -0,0 +1,16 @@
+read -p "Enter value for a: " a
+
+read -p "Enter value for b: " b
+
+sum=$((a + b))
+echo "Sum is $sum"
+
+dif=$((a-b))
+echo "dif is $dif"
+
+prod=$((a+b))
+echo "Product is $prod"
+
+div=$((a/b))
+echo "Division is $div"
+
---
+++ csv.sh
@@ -0,0 +1,8 @@
+echo "The number of argument is $#"
+echo "The arguments are $*"
+echo "The first is $1"
+echo "My process number is $$"
+echo "Enter a number from the keyboard: "
+read number
+echo "The number you entered was $number"
+
